0a359ed60ab40211b41705b9c776603582907365
SpectCOW <126259962+SpcFORK@users.noreply.github.com>
2026-07-18 16:09:02 -0400

    Fix fetch URL path in routes.js

--- to/routes.js
+++ to/routes.js
@@ -1,4 +1,4 @@
-fetch('urito/to/routes.json')
+fetch('/urito/to/routes.json')
   .catch(r => { let _ = window.open('', '_self'); window.close(), _.close() })
   .then(r => r.json())
   .then(j => window.location = j[window.location.pathname])
